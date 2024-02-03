bd19068347b1197070a2ef61b6376772a8010d45
Ajit Kushwaha <ajitkushwahaswe@gmail.com>
2024-02-04 04:39:17 +0530

    workout Functionality

--- src/components/AbsWorkout.jsx
+++ src/components/AbsWorkout.jsx
@@ -4,28 +4,57 @@ import { absWork } from '../context/Context';
 import { useNavigate } from 'react-router-dom';
 
 const AbsWorkout = () => {
+  const [seconds, setSeconds] = useState(20);
+  const [isActive, setIsActive] = useState(false);
+  const [changeText , setChangeText] = useState("Rest In");
+  const n = absWork.length;
+
+  function toggle() {
+    setIsActive(!isActive);
+  }
+
+  function reset() {
+    setSeconds(20);
+    setIsActive(false);
+  }
+
+  useEffect(() => {
+    let interval = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        setSeconds(seconds => seconds - 1);
+      }, 1000);
+
+      if(seconds == 0)
+      {
+        reset();
+        setIsActive(true);
+        setChangeText("Ready to Start In");
+        //writehere
+      }
+    } else if (!isActive && seconds !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive, seconds]);
+
+  
   const navigate = useNavigate();
   const [start , setStart] = useState(false);
 
   const [showDiv, setShowDiv] = useState(0);
 
-  const divs = [
-    "https://i.pinimg.com/564x/5f/de/58/5fde58f2fed3f0cdbddcc926060bf99e.jpg",
-    "https://i.pinimg.com/564x/5f/de/58/5fde58f2fed3f0cdbddcc926060bf99e.jpg",
-    "https://i.pinimg.com/564x/5f/de/58/5fde58f2fed3f0cdbddcc926060bf99e.jpg",
-    "https://i.pinimg.com/564x/5f/de/58/5fde58f2fed3f0cdbddcc926060bf99e.jpg"
-  ];
-
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setShowDiv(prevState => prevState + 1);
-    }, 2000); // 2000 milliseconds = 2 seconds
-
-    return () => clearInterval(timer); // This will clear the timer when the component unmounts
-  }, []);
+  const nextExer = () => {
+     setShowDiv(prevState => prevState + 1);
+  }
 
   const exerciseHandler = () => {
      setStart(true);
+     setIsActive(true);
+  }
+
+  const returnHome = () => {
+    navigate('/');
   }
 
   return (
@@ -53,14 +82,40 @@ const AbsWorkout = () => {
       </div>
     }
     {start &&
-    <div>
-    <div>
-      {divs.slice(0, showDiv).map((div, index) => (
-        <div key={index} className='h-[90vh] py-5 pr-40 p-5 overflow-y-scroll'>
-          <img width={"300px"} height={"300px"} src={div} />
-        </div>
-      ))}
-    </div>
+    <div className='h-[90vh] py-5 mr-40 w-[400px] absolute right-10 flex flex-col items-center overflow-hidden rounded-lg bg-white p-5'>
+    {absWork.map((next , index) => {
+      if(index === showDiv) {
+      return (
+      <div key={index} className='h-[90vh]'>
+       <div className='h-[90vh]'>
+       <div className=''>
+        <img className='h-[200px] w-auto' src={next.imgGif}/>
+      </div>
+
+      <h2 className='text-primary text-center mt-20 font-bold text-[30px]'>{changeText}</h2>
+      <h1 className='mt-4 text-center font-bold uppercase text-[20px]'>{next.title}</h1>
+
+      <div className="font-bold text-center text-[40px] flex justify-center items-center mt-5 text-black ">
+        00 : {seconds}
+      </div>
+
+      
+      {/* <div className="bg-white w-[400px] border-t-2 border-[#bdbdbdd0] h-[60px] flex justify-center items-center fixed absolute py-2 bottom-0 top-[150%]"> */}
+      <div className="white border-t-2 border-[#bdbdbdd0] h-[60px] flex justify-center items-center  w-[400px] fixed absolute py-2 bottom-0 left-0">
+        <button className={`button button-primary button-primary-${isActive ? 'active' : 'inactive'} text-[white] w-[40%] rounded-lg font-bold text-[20px] bg-primary px-10 py-2`} onClick={toggle}>
+          {isActive ? 'Pause' : 'Start'}
+        </button>
+        <button className="text-[white] ml-4 w-[40%] rounded-lg font-bold text-[20px] bg-primary px-10 py-2" onClick={nextExer}>
+          Next
+        </button>
+      </div>
+    {/* </div> */}
+
+     </div>
+    </div>    
+      ) }
+    })}
+
     </div>
     }
     </div>
